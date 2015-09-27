e91650827d7f2179d290687c211261cd065a59c3
Sarah Alsmiller <ramvling@gmail.com>
2015-09-27 04:50:23 -0400

    merging

--- ship_client.js
+++ ship_client.js
@@ -121,8 +121,8 @@ function startConnection() {
     }
 
     // Start the connection!
-    wsconnect("ws://128.61.70.189:8886", onopen, onclose);
-    //wsconnect("ws://localhost:8886", onopen, onclose);
+    //wsconnect("ws://128.61.70.189:8886", onopen, onclose);
+    wsconnect("ws://localhost:8886", onopen, onclose);
 }
 
 function handleGameOver(msg) {
